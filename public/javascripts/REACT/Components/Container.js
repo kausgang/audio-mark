845e76e94191b7d188e829048059a8731f66a8e8
Kaustav Ganguli <33816465+kausgang@users.noreply.github.com>
2022-05-03 14:37:16 -0400

    seek 10

--- public/javascripts/REACT/Components/Container.js
+++ public/javascripts/REACT/Components/Container.js
@@ -1,7 +1,7 @@
 function Container(props) {
   // const [value, setValue] = React.useState(0);
   const [slider_value,setSlider_value]=React.useState(0)
-  // const [audio_value,setAudio_value]=React.useState(0)
+  const [audio_value,setAudio_value]=React.useState(0)
 
   //THIS IS FOR MANUAL SLIDER MOVEMENT
   function slider_handler(newValue) {
@@ -24,7 +24,7 @@ function Container(props) {
       <ProgressBar sound={props.sound} slider_move={slider_handler} current_seek={slider_value} />
       {/* SHOW THE SLIDER VALUE */}
       <p>{slider_value}</p>
-      <PlaybackControl seek_progressbar={slider_handler} current_seek={slider_value} sound={props.sound}/>
+      <PlaybackControl seek_progressbar={slider_handler} current_seek={slider_value} audio_value={audio_value} sound={props.sound}/>
       {/* <PlaybackControl  current_seek={slider_value} sound={props.sound}/> */}
     </div>
   );
--- public/javascripts/REACT/Components/PlaybackControl.js
+++ public/javascripts/REACT/Components/PlaybackControl.js
@@ -5,39 +5,31 @@ function PlaybackControl(props) {
   // initialize timestamp to 0 second
   const [timestamp, setTimestamp] = React.useState(0);
   const [playing,setPlaying]= React.useState(false)
-  
-  // let intervalID = 0
+
  
 
   // MOVING SLIDER WILL UPDATE CURRENTSEEK AND RERENDER PLAYBACK
   React.useEffect(()=>{
 
     setTimestamp(props.current_seek)
-    // sound.seek(timestamp)
+    
+    
+    if(playing){
+      
+      setTimeout(() => {
+        props.seek_progressbar(Math.round(sound.seek()))
+      }, 1000);
+    }
+
+
   })
 
 
   const play = () => {
 
-    let intervalID = 0
+    // let intervalID = 0
 
-    // // GET CURRENT SEEK TIME FROM PARENT
-    // setTimestamp(props.current_seek)
-    // console.log('timestamp = ',timestamp)
     
-    // seek
-    // sound.seek(timestamp);
-    // start playback
-    // sound.play();
-
-
-
-
-    // // START THE PLAYBACK COUNTER
-    // intervalID=setInterval(() => {    
-    //   // CALL THE PARENT SEEK FUNCTION TO UPDATE SLIDER
-    //     props.seek_progressbar(Math.round(sound.seek()))
-    // }, 1000); //do it every 1 second
 
 
     // UPDATE TIMESTAMP ONLY WHEN IT IS PLAYING
@@ -46,7 +38,7 @@ function PlaybackControl(props) {
     if(button_class==='playing') //now i can only pause it
     {
       // CLEAR INTERVAL
-      clearInterval(intervalID) 
+      // clearInterval(intervalID) 
       // PAUSE THE MUSIC
       sound.pause()
       //CHANGE CLASSNAME
@@ -59,16 +51,12 @@ function PlaybackControl(props) {
       // SEEK TO TMESTAMP
       sound.seek(timestamp);
       
-      // START THE PLAYBACK COUNTER
-      intervalID = setInterval(() => {    
-      // CALL THE PARENT SEEK FUNCTION TO UPDATE SLIDER
-        props.seek_progressbar(Math.round(sound.seek()))
-    }, 1000); //do it every 1 second
+
 
 
       // PLAY MUSIC
       sound.play()
-      //CHANGE CLASSNAME
+      //CHANGE CLASSNAME TO RERENDER COMPONENT
       document.getElementById("play_pause").setAttribute('class','playing')
     }
 
@@ -77,27 +65,36 @@ function PlaybackControl(props) {
   };
 
 
-  const stop = () => {
-    sound.stop();
-  };
+  // const stop = () => {
+  //   let button_class = document.getElementById("play_pause").getAttribute('class')
+    
+  //   if(button_class==='playing') //now i can only pause it
+  //   {document.getElementById("play_pause").setAttribute('class','paused')}
+    
+  //   // CHANGE BUTTON TEXT 
+  //   // playing?setPlaying(false):setPlaying(true)
+  //   console.log("playing = " , playing)
+  //   setPlaying(false)
+
+  //   // SEEK TO 0
+  //   sound.seek(0)
+  //   // props.seek_progressbar(sound.seek(0))
+
+  //   sound.stop();
+  // };
 
   const seek10 = () => {
-    // get the current time
-    const time = sound.seek();
-    // stop the playback
-    sound.stop();
-    // seek
-    sound.seek(time + 10);
-    // start playback
-    sound.play();
-    // show timestamp
-    setInterval(() => {
-      // update timestamp variable
-      setTimestamp(() => {
-        //roundup the seek value to show passed seconds
-        return Math.round(sound.seek());
-      });
-    }, 1000);
+
+
+    // GET CURRENT SEEK TIME FROM PARENT
+    setTimestamp(props.current_seek)
+    // SEEK TO TMESTAMP
+    sound.seek(timestamp+10);
+
+    // UPDATE THE SEEK COUNTER IN PROGRESS BAR
+    props.seek_progressbar(timestamp+10)
+
+  
   };
 
   return (
@@ -107,7 +104,7 @@ function PlaybackControl(props) {
       {/* <br></br> */}
       <button id="play_pause" className='paused' onClick={play}>Click to {playing?'Pause':'Play'}</button>
       {/* <button onClick={pause}>Click to pause</button> */}
-      <button onClick={stop}>Click to stop</button>
+      {/* <button onClick={stop}>Click to stop</button> */}
       <button onClick={seek10}>Forward 10sec</button>
     </div>
   );
