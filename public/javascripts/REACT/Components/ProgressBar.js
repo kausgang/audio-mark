a2fd3a95e7245a45cfa7d92aa3a3b0c391a783ac
kausgang <write.to.kaustav@gmail.com>
2022-05-02 21:07:42 -0400

    progressbar

---
+++ public/javascripts/REACT/Components/ProgressBar.js
@@ -0,0 +1,7 @@
+function ProgressBar() {
+  return (
+    <div>
+      <Slider />
+    </div>
+  );
+}
---
+++ public/javascripts/REACT/index.js
@@ -0,0 +1,7 @@
+const rootelement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootelement);
+root.render(
+  <div>
+    <ProgressBar />
+  </div>
+);
