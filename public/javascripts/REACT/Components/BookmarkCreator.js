539f9909517d8cef9c798c98c02024605f8a1562
kausgang <write.to.kaustav@gmail.com>
2022-05-03 17:53:32 -0400

    fs api

--- public/javascripts/REACT/Components/BookmarkCreator.js
+++ public/javascripts/REACT/Components/BookmarkCreator.js
@@ -1,14 +1,13 @@
-
 function BookmarkCreator(props) {
-    const create_bookmark=()=>{
-        console.log(props.timestamp)
-        let name=prompt("What's the name for the Bookmark")
-        props.save_bookmark(name)
-        alert("Bookmark added")
-    }
+  const create_bookmark = () => {
+    console.log(props.timestamp);
+    let name = prompt("What's the name for the Bookmark");
+    props.save_bookmark(name);
+    // alert("Bookmark added")
+  };
   return (
     <div>
-        <button onClick={create_bookmark}>Create Bookmark</button>
+      <button onClick={create_bookmark}>Create Bookmark</button>
     </div>
-  )
+  );
 }
--- public/javascripts/REACT/Components/Container.js
+++ public/javascripts/REACT/Components/Container.js
@@ -1,30 +1,45 @@
 function Container(props) {
-
-  const [slider_value,setSlider_value]=React.useState(0)
-  const [audio_value,setAudio_value]=React.useState(0)
+  const [slider_value, setSlider_value] = React.useState(0);
+  const [audio_value, setAudio_value] = React.useState(0);
 
   //THIS IS FOR MANUAL SLIDER MOVEMENT
   function slider_handler(newValue) {
-
-    setSlider_value(newValue)
-
+    setSlider_value(newValue);
   }
 
-
-  function save_bookmark(name){
-    console.log(name)
+  function save_bookmark(name) {
+    console.log(name);
     //API call to TO SAVE IT IN SERVER
+    fetch("/uses").then((response) => {
+      const status = response.status;
+      console.log("status = ", status);
+
+      if (status === 404) {
+        alert("error occured writing to bookmark record");
+      } else {
+        alert("Bookmark created successfully");
+      }
+    });
   }
 
   return (
     <div>
       {/* SEND THE SOUND VARIABLE TO IT'S CHILDREN */}
-      <ProgressBar sound={props.sound} slider_move={slider_handler} current_seek={slider_value} />
+      <ProgressBar
+        sound={props.sound}
+        slider_move={slider_handler}
+        current_seek={slider_value}
+      />
       {/* SHOW THE SLIDER VALUE */}
       <p>{slider_value}</p>
-      <PlaybackControl seek_progressbar={slider_handler} current_seek={slider_value} audio_value={audio_value} sound={props.sound}/>
+      <PlaybackControl
+        seek_progressbar={slider_handler}
+        current_seek={slider_value}
+        audio_value={audio_value}
+        sound={props.sound}
+      />
       <br></br>
-      <BookmarkCreator timestamp={slider_value} save_bookmark={save_bookmark}/>
+      <BookmarkCreator timestamp={slider_value} save_bookmark={save_bookmark} />
     </div>
   );
 }
