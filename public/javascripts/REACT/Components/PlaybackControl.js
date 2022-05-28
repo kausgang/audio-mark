436b47787e6a8f9f4e9dfbfea4680c7b866df531
kausgang <write.to.kaustav@gmail.com>
2022-05-27 20:50:28 -0400

    Update PlaybackControl.js

--- public/javascripts/REACT/Components/PlaybackControl.js
+++ public/javascripts/REACT/Components/PlaybackControl.js
@@ -175,7 +175,7 @@ function PlaybackControl(props) {
 
   return (
     <div>
-      <div className="btn-group mr-3" role="group" aria-label="Basic example">
+      <div className="btn-group" role="group" aria-label="Basic example">
         {/* back 60 sec */}
         <button type="button" className="btn btn-secondary" onClick={back60sec}>
           <span className="material-symbols-rounded">arrow_back</span>
